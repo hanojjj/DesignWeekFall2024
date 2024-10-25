07ffe0b1fe5d94096c8e04c51235017f9811a267
Jonah Cappa <jonahcappaofficial@gmail.com>
2024-10-24 23:09:05 -0400

    Update player movement

--- DesignWeekFall2024/Assets/Scripts/PlayerMovement.cs
+++ DesignWeekFall2024/Assets/Scripts/PlayerMovement.cs
@@ -9,34 +9,51 @@ public class PlayerMovement : MonoBehaviour
     public Transform transform;
 
     public float movementSpeed = 0.5f;
-    //public Vector3 playerStartPos;
+    public Vector3 playerStartPos;
 
     private void Start()
     {
-        //playerStartPos = new Vector3(16.1f, 2.35f, -16.24f);
+        playerStartPos = new Vector3(22.8260002f, 1.53900003f, -8.89000034f);
+        
     }
 
 
 
     void Update()
     {
-        MoveRight();
+        Move();
+        
     }
 
     //Static Movements
+    public void Move()
+    {
+        if (transform.position.x < 60.5f)
+        {
+            MoveRight();
+            //transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            MoveBackwards();
+            //transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        transform.position = playerStartPos;
+    }
+
+
+
+
+
+    //Movements
     public void MoveRight()
     {
         transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
-    }
-
-    public void MoveLeft()
-    {
-        transform.position = transform.position + new Vector3(-movementSpeed * Time.deltaTime, 0, 0);
-    }
-
-    public void MoveForward()
-    {
-        transform.position = transform.position + new Vector3(0, 0, movementSpeed * Time.deltaTime);
     }
 
     public void MoveBackwards()
@@ -44,5 +61,7 @@ public class PlayerMovement : MonoBehaviour
         transform.position = transform.position + new Vector3(0, 0, -movementSpeed * Time.deltaTime);
 
     }
+
+    
 
 }
