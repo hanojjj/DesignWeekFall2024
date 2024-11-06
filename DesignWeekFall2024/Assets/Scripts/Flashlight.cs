2f929ca7c645c8a3c9baff2ce0aea25d6cf6415d
Jonah Cappa <jonahcappaofficial@gmail.com>
2024-11-06 16:52:54 -0500

    Update script

--- DesignWeekFall2024/Assets/Scripts/Flashlight.cs
+++ DesignWeekFall2024/Assets/Scripts/Flashlight.cs
@@ -24,4 +24,8 @@ public class Flashlight : MonoBehaviour
         flashLight.enabled = !flashLight.enabled;
         source.PlayOneShot(light);
     }
+
+
+
+    ///alsjdhfisakdgf;asidoubg'aosdubfsadfsadf
 }
